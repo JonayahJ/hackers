2d54c73d66ebc41b768298704e7548d46db08d92
Jonayah Jackson <jonayah@thinkhalcyon.com>
2020-11-05 17:50:44 -0500

    made it easier for Xavier

--- hackers.cpp
+++ hackers.cpp
@@ -2,28 +2,24 @@
 
 int main()
 {
-    std::cout << "Welcome, Hacker.";
-    std::cout << std::endl;
-    std::cout << "ZeroCool needs your help!  It is up to you to enter the correct codes to save the tanker from a massive oil spill.  In order to stop this mayhem from ensuing, you must enter the correct codes to defeat The Plague.";
-    std::cout << std::endl;
+    std::cout << "Welcome, Hacker. ZeroCool needs your help!  \nIt is up to you to enter the correct codes to save the tanker from a massive oil spill.  \nIn order to stop this mayhem from ensuing, you must enter the correct codes to defeat The Plague." << std::endl;
 
-    const int CodeA = 3;
-    const int CodeB = 9;
-    const int CodeC = 27;
+    const int CodeA = 1;
+    const int CodeB = 3;
+    const int CodeC = 5;
 
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProd = CodeA * CodeB * CodeC;
     
+    // Print sum and product to the terminal
     std::cout << std::endl;
-    std::cout << "Here are some hints:" << std::endl;
-    std::cout << "There are three numbers in the code." << std::endl;
-    std::cout << "The codes add up to: " << CodeSum << std::endl;
+    std::cout << "Here are some hints: \nThere are three numbers in the code. \nThe codes add up to: " << CodeSum <<std::endl;
     std::cout << "The codes multiply to: " << CodeProd << std::endl;
 
     int GuessA, GuessB, GuessC;
 
-    std::cout << "Now it's time for you to start entering codes.  Remember to only enter numbers." <<std::endl;
-    std::cout << "Enter the code for the first number: "; 
+    // Store player guess
+    std::cout << "Now it's time for you to start entering codes.  Remember to only enter numbers. \nEnter the code for the first number: "; 
     std::cin >> GuessA;
     std::cout << "Enter the code for the second number: "; 
     std::cin >> GuessB;
@@ -34,6 +30,7 @@ int main()
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProd = GuessA * GuessB * GuessC;
 
+    // Check if guess is correct
     if(GuessSum == CodeSum && GuessProd == CodeProd)
     {
         std::cout << "Hack the planet!  You saved the tanker!  Good job, Hacker.";
