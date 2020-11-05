192c1726f838e6e81330a4bd08cdcda32f510d9d
Jonayah Jackson <jonayah@thinkhalcyon.com>
2020-11-05 15:53:31 -0500

    working with variables

--- hackers.cpp
+++ hackers.cpp
@@ -1,4 +1,23 @@
+#include <iostream>
+
 int main()
 {
+    std::cout << "Welcome, ZeroCool.";
+    std::cout << std::endl;
+    std::cout << "It is up to you to save the tanker from a massive oil spill.  In order to stop this mayhem from ensuing, you must enter the correct codes to defeat The Plague.";
+    std::cout << std::endl;
+
+    int a = 4;
+    int b = 9;
+    int c = 23;
+
+    a = 3;
+
+    int sum = a + b + c;
+    int prod = a * b * c;
+
+    std::cout << sum << std::endl;
+    std::cout << prod;
+
     return 0;
 }
