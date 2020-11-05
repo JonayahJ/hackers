cc08c80c1fc54cce04fc286c1a97c4fd23027bc7
Jonayah Jackson <jonayah@thinkhalcyon.com>
2020-11-05 16:58:03 -0500

    added in var for player guess

--- hackers.cpp
+++ hackers.cpp
@@ -2,20 +2,25 @@
 
 int main()
 {
-    std::cout << "Welcome, ZeroCool.";
+    std::cout << "Welcome, Hacker.";
     std::cout << std::endl;
-    std::cout << "It is up to you to save the tanker from a massive oil spill.  In order to stop this mayhem from ensuing, you must enter the correct codes to defeat The Plague.";
+    std::cout << "ZeroCool needs your help!  It is up to you to enter the correct codes to save the tanker from a massive oil spill.  In order to stop this mayhem from ensuing, you must enter the correct codes to defeat The Plague.";
     std::cout << std::endl;
 
-    const int a = 3;
-    const int b = 9;
-    const int c = 27;
+    const int CodeA = 3;
+    const int CodeB = 9;
+    const int CodeC = 27;
 
-    const int sum = a + b + c;
-    const int prod = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProd = CodeA * CodeB * CodeC;
+    
+    std::cout << std::endl;
+    std::cout << "Here are some hints:" << std::endl;
+    std::cout << "There are three numbers in the code." << std::endl;
+    std::cout << "The codes add up to: " << CodeSum << std::endl;
+    std::cout << "The codes multiply to: " << CodeProd << std::endl;
 
-    std::cout << sum << std::endl;
-    std::cout << prod;
+    int PlayerGuess;
 
     return 0;
 }
