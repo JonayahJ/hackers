d5aeefffa7be060be4075088af1c987096acc2ac
Jonayah Jackson <jonayah@thinkhalcyon.com>
2020-11-07 10:45:02 -0500

    added level difficulty thru params

--- hackers.cpp
+++ hackers.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
 // void means the fx will return no data
-void PrintIntroduction()
+void PrintIntroduction(int Difficulty)
 {
-    // Intro messages
-    std::cout << "Welcome, Hacker. ZeroCool needs your help!  \nIt is up to you to enter the correct codes to save the tanker from a massive oil spill.  \nIn order to stop this mayhem from ensuing, you must enter the correct codes to defeat The Plague. \n\n";
+    std::cout << "\n\nWelcome, Hacker. ZeroCool needs your help!  \nIt is up to you to enter the correct codes to save the tanker from a massive oil spill.  Right now, you're hacking into a Level " << Difficulty;
+    std::cout << " Terminal.  \nIn order to stop this mayhem from ensuing, you must enter the correct codes to defeat The Plague.  Make it all the way to Level 10, and you will have saved the day!\n\n";
 }
 
-void PlayGame()
+bool PlayGame(int Difficulty)
 {
     // Call my fx
-    PrintIntroduction();
+    PrintIntroduction(Difficulty);
     
     // Code Variables
     const int CodeA = 1;
@@ -22,13 +22,13 @@ void PlayGame()
     
     // Print sum and product to the terminal
     std::cout << std::endl;
-    std::cout << "Here are some hints: \nThere are three numbers in the code. \nThe codes add up to: " << CodeSum <<std::endl;
-    std::cout << "The codes multiply to: " << CodeProd << std::endl;
+    std::cout << "Here are some hints: \n+ There are three numbers in the code. \n+ The codes add up to: " << CodeSum <<std::endl;
+    std::cout << "+ The codes multiply to: " << CodeProd << std::endl;
 
     int GuessA, GuessB, GuessC;
 
     // Store player guess
-    std::cout << "Now it's time for you to start entering codes.  Remember to only enter numbers. \nEnter the code for the first number: "; 
+    std::cout << "\n\nNow it's time for you to start entering codes.  Remember to only enter numbers. \n\nEnter the code for the first number: "; 
     std::cin >> GuessA;
     std::cout << "Enter the code for the second number: "; 
     std::cin >> GuessB;
@@ -42,16 +42,32 @@ void PlayGame()
     // Check if guess is correct
     if(GuessSum == CodeSum && GuessProd == CodeProd)
     {
-        std::cout << "Hack the planet!  You saved the tanker!  Good job, Hacker.";
+        std::cout << "\nHack the planet!  You saved the tanker!  Good job, Hacker.\n";
+        return true;
     }
     else
     {
-        std::cout << "Bad smarts.  The tanker sank and the oil refinery has been destroyed.  Better luck next time.";
+        std::cout << "\nBad smarts.  The tanker sank and the oil refinery has been destroyed.  Better luck next time.\n";
+        return false;
     }
 }
 
 int main()
 {
-    PlayGame();
+    int LevelDifficulty = 1;
+
+    while(true)
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear(); 
+        std::cin.ignore();
+
+        if (bLevelComplete)
+        {
+            ++LevelDifficulty;
+        }
+        
+    }
+    
     return 0;
 }
